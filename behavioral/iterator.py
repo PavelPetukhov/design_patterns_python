2dfb567cd68fbfe1e164f0e89dce163d4fd49773
Pasha Petukhov <pavpetukhov@gmail.com>
2020-02-07 17:35:03 -0800

    minor decription

--- behavioral/iterator.py
+++ behavioral/iterator.py
@@ -1,5 +1,10 @@
 from collections.abc import Iterable, Iterator
 
+# Provide a way to access the elements of an aggregated object sequentially
+# without exposing its underlying representation
+# The key idea in this pattern is to take the responsibility for access and
+# traversal out of the list object and put it into an iterator object
+
 
 class CustomCollection:
     def __init__(self, collection_):
